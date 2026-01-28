d8d7970ed6c3ffaa8c59d36b9211450421f2274b
gksleepy <165460305+gksleepy@users.noreply.github.com>
2026-01-28 12:40:50 +0000

    add add_two function

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
 # print hello world
 print("hello world")
+
+# print good bye
+print("good bye")
+
+# print new line
+print("i'm learinig github.")
+
+# new function
+def add_two(num):
+    return num + 2
+
+print(add_two(2),add_two(5))
